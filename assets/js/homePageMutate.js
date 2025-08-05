dc4f033257c1f80acb8f1cac415dcf63db373a27
Will Andree <williamandree@gmail.com>
2025-08-05 11:35:36 -0500

    Housekeeping.

--- assets/js/homePageMutate.js
+++ assets/js/homePageMutate.js
@@ -2,30 +2,30 @@ const courseDetailsQuery =
   "https://fhm6z54x.api.sanity.io/v2025-07-31/data/query/production?query=*%5B_type+%3D%3D+%27courseDetails%27%5D%5B0%5D&perspective=published";
 
 document.addEventListener("DOMContentLoaded", function () {
+  // cornered box
+  const corneredBox = document.getElementsByClassName("cornered-box")[0];
+  const alert = document.getElementsByClassName("frost-warning")[0];
+  const alertTitle = document.getElementsByClassName("frost-warning-title")[0];
+  const alertText = document.getElementsByClassName("frost-warning-text")[0];
+
+  // pool sign
+  const poolEl = document.getElementsByClassName("pool")[0];
+  const poolSign = document.getElementsByClassName("closed-sign")[0];
+
   //~ fetch data
   fetchData(courseDetailsQuery).then((data) => {
     const courseDetails = data;
     console.log(JSON.stringify(courseDetails, null, 2));
     if (courseDetails?.alertQuestion) {
-      const corneredBox = document.getElementsByClassName("cornered-box")[0];
-      const alert = document.getElementsByClassName("frost-warning")[0];
-      const alertTitle = document.getElementsByClassName(
-        "frost-warning-title"
-      )[0];
-      const alertText =
-        document.getElementsByClassName("frost-warning-text")[0];
       corneredBox.style = "display: none";
       alert.style = "display: block";
       alertTitle.textContent = courseDetails?.alert.alertHeading;
       alertText.textContent = courseDetails?.alert.alertTextBody;
-    } else if (!courseDetails?.poolQuestion) {
-      // runs this code if poolQuestion = false or "POOL IS CLOSED"
-      const poolEl = document.getElementsByClassName("pool")[0];
-      const poolSign = document.getElementsByClassName("closed-sign")[0];
-      poolEl.classList.toggle("closed");
+    }
+    if (!courseDetails?.poolQuestion) {
+      // runs this code if poolQuestion = false or "THE POOL IS CLOSED"
+      poolEl.classList.add("closed");
       poolSign.style = "display: unset;";
-    } else {
-      return;
     }
   });
 });
